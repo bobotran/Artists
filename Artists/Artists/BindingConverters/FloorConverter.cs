d8094d7c22b50f335e8775b22b9b3b0723ea0622
Kevin Tran <320trankt@gmail.com>
2018-06-29 00:47:55 -0700

    Floor Converter method

--- Artists/Artists/BindingConverters/FloorConverter.cs
+++ Artists/Artists/BindingConverters/FloorConverter.cs
@@ -16,7 +16,83 @@ namespace Artists.BindingConverters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             int floorInt = (int)value;
-            return "";
+            String floor;
+            switch (floorInt)
+            {
+              case 0: return "";
+              case 1: floor = floorInt.toString() + "st Floor";
+                      return floor;
+              case 2: floor = floorInt.toString() + "nd Floor";
+                      return floor;
+              case 3: floor = floorInt.toString() + "rd Floor";
+                      return floor;
+              case 11: floor = floorInt.toString() + "th Floor";
+                        return floor;
+              case 12: floor = floorInt.toString() + "th Floor";
+                        return floor;
+              case 13: floor = floorInt.toString() + "th Floor";
+                        return floor;
+              case 10: floor = floorInt.toString() + "th Floor";
+                        return floor;
+            }
+            int newFloorInt = (floorInt % 10);
+            switch (newFloorInt)
+            {
+              case 1: floor = floorInt.toString() + "st Floor";
+                      return floor;
+              case 2: floor = floorInt.toString() + "nd Floor";
+                      return floor;
+              case 3: floor = floorInt.toString() + "rd Floor";
+                      return floor;
+            }
+            return (floorInt.toString() + "th Floor");
+
+
+
+            // if (floorInt > 10)
+            // {
+            //   if (floorInt < 14)
+            //   {
+            //     floor = floorInt.toString() + "th Floor";
+            //   }
+            //   else if ((floorInt % 10) == 1)
+            //   {
+            //     floor = floorInt.toString() + "st Floor";
+            //   }
+            //   else if ((floorInt % 10) == 2)
+            //   {
+            //     floor = floorInt.toString() + "nd Floor";
+            //   }
+            //   else if ((floorInt % 10) == 3)
+            //   {
+            //     floor = floorInt.toString() + "rd Floor";
+            //   }
+            //   else
+            //   {
+            //     floor = floorInt.toString() + "th Floor";
+            //   }
+            // }
+            //
+            // else if (floorInt == 1)
+            // {
+            //   floor = floorInt.toString() + "st Floor";
+            // }
+            //
+            // else if (floorInt == 2)
+            // {
+            //   floor = floorInt.toString() + "nd Floor";
+            // }
+            //
+            // else if (floorInt == 3)
+            // {
+            //   floor = floorInt.toString() + "rd Floor";
+            // }
+            //
+            // else
+            // {
+            //   floor = floorInt.toString() + "th Floor";
+            // }
+
         }
         //Don't implement
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
